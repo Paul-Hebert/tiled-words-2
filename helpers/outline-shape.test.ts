31c8721bb9cc8e3721d3ba1cd0eb103a674d3249
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2025-07-19 10:34:50 -0700

    Test fully wrapped cells

--- helpers/outline-shape.test.ts
+++ helpers/outline-shape.test.ts
@@ -36,6 +36,31 @@ describe('outlineShape', () => {
     expect(outlineShape(grid)).toEqual(expected)
   })
 
+  it('should outline a 3x3 square', () => {
+    const grid: Grid = [
+      ['a', 'b', 'c'],
+      ['d', 'e', 'f'],
+      ['g', 'h', 'i'],
+    ]
+
+    const expected = [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 3, y: 0 },
+      { x: 3, y: 1 },
+      { x: 3, y: 2 },
+      { x: 3, y: 3 },
+      { x: 2, y: 3 },
+      { x: 1, y: 3 },
+      { x: 0, y: 3 },
+      { x: 0, y: 2 },
+      { x: 0, y: 1 },
+    ]
+
+    expect(outlineShape(grid)).toEqual(expected)
+  })
+
   it('should outline an L shape', () => {
     const grid: Grid = [
       ['a', null, null],
