b11102e1302027651fb40106df72427315f4ade3
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2025-07-24 08:40:45 -0700

    New level

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,94 @@ const levels = [
       {
         id: 'tile-1',
         position: {
-          x: 3,
-          y: 3,
+          x: 1,
+          y: 2,
         },
         grid: padGridToSquare([
-          [null, 's', null],
-          ['p', 'a', 'r'],
+          ['s', 'h', 'a'],
+          [null, null, 'r'],
+          [null, null, 'k'],
+        ]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-2',
+        position: {
+          x: 3,
+          y: 0,
+        },
+        grid: padGridToSquare([
+          ['j', 'e', 'l', 'l', 'y'],
+          [null, null, null, null, 'f'],
+          [null, null, null, null, 'i'],
+          [null, null, null, null, 's'],
+          [null, null, null, null, 'h'],
+        ]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-3',
+        position: {
+          x: 5,
+          y: 6,
+        },
+        grid: padGridToSquare([['c', 'r', 'a', 'b']]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-4',
+        position: {
+          x: 0,
+          y: 3,
+        },
+        grid: padGridToSquare([['w'], ['h'], ['a'], ['l'], ['e']]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-5',
+        position: {
+          x: 3,
+          y: 4,
+        },
+        grid: padGridToSquare([['y'], ['e'], ['l']]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-6',
+        position: {
+          x: 6,
+          y: 8,
+        },
+        grid: padGridToSquare([['l']]),
+        rotations: 0,
+      },
+    ],
+  },
+  {
+    id: 'level-2',
+    theme: 'Sea animals',
+    gridSize: 12,
+    words: {
+      vertical: [
+        { text: 'whale', hint: 'A _____ of a time!' },
+        { text: 'seal', hint: 'A sea dog' },
+      ],
+      horizontal: [
+        { text: 'shark', hint: 'Apex predator' },
+        { text: 'jellyfish', hint: 'Peanut butter and...' },
+        { text: 'crab', hint: 'Sea spider' },
+      ],
+    },
+    tiles: [
+      {
+        id: 'tile-1',
+        position: {
+          x: 6,
+          y: 2,
+        },
+        grid: padGridToSquare([
+          [null, 'w', null],
+          ['s', 'h', 'a'],
         ]),
         rotations: 1,
       },
@@ -39,57 +121,82 @@ const levels = [
         id: 'tile-2',
         position: {
           x: 1,
-          y: 6,
+          y: 3,
         },
-        grid: padGridToSquare([['r', 'o', 's']]),
+        grid: padGridToSquare([
+          ['r', 'k'],
+          ['a', null],
+        ]),
         rotations: 2,
       },
       {
         id: 'tile-3',
         position: {
-          x: 3,
+          x: 8,
           y: 8,
         },
-        grid: padGridToSquare([
-          ['g', null],
-          ['e', 'm'],
-        ]),
-        rotations: 3,
+        grid: padGridToSquare([['j', 'e']]),
+        rotations: 0,
       },
       {
         id: 'tile-4',
         position: {
-          x: 6,
-          y: 7,
+          x: 8,
+          y: 0,
         },
-        grid: padGridToSquare([['a', 'r', 'y']]),
+        grid: padGridToSquare([
+          ['a', null],
+          ['l', 'l'],
+          ['e', null],
+        ]),
         rotations: 0,
       },
       {
         id: 'tile-5',
         position: {
-          x: 6,
-          y: 0,
+          x: 0,
+          y: 8,
         },
-        grid: padGridToSquare([['t'], ['h'], ['y']]),
-        rotations: 1,
+        grid: padGridToSquare([['y', 'f', 'i']]),
+        rotations: 0,
       },
       {
         id: 'tile-6',
         position: {
-          x: 7,
-          y: 4,
+          x: 4,
+          y: 0,
         },
         grid: padGridToSquare([
-          [null, null, 'm', null],
-          ['s', 'l', 'e', 'y'],
+          ['s', 'h'],
+          ['e', null],
         ]),
         rotations: 1,
+      },
+      {
+        id: 'tile-7',
+        position: {
+          x: 4,
+          y: 3,
+        },
+        grid: padGridToSquare([['c', 'r']]),
+        rotations: 0,
+      },
+      {
+        id: 'tile-8',
+        position: {
+          x: 4,
+          y: 6,
+        },
+        grid: padGridToSquare([
+          ['a', 'b'],
+          ['l', null],
+        ]),
+        rotations: 0,
       },
     ],
   },
   {
-    id: 'level-2',
+    id: 'level-3',
     theme: 'Fruits and Veggies',
     gridSize: 12,
     words: {
@@ -204,7 +311,7 @@ const levels = [
   },
 ]
 
-const currentLevelIndex = ref(0)
+const currentLevelIndex = ref(1)
 const currentLevel = computed(() => levels[currentLevelIndex.value])
 
 const nextLevel = () => {
