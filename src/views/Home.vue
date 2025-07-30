dfc8988cc89b2847cb44704e20b9b5980cf9c1f9
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2025-07-29 17:29:10 -0700

    Improve home responsive design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@ import LogoExample from '@/components/instructions/LogoExample.vue'
 
 <template>
   <div class="home-container">
+    <div role="heading" aria-level="1" aria-label="Tiled Words" class="logo">
+      <LogoExample />
+    </div>
     <div class="content">
-      <div role="heading" aria-level="1" aria-label="Tiled Words">
-        <LogoExample />
-      </div>
       <p class="subtitle">Reconstruct crosswords out of tiles</p>
       <Button href="/instructions">Get Started</Button>
 
@@ -25,16 +25,19 @@ import LogoExample from '@/components/instructions/LogoExample.vue'
 .home-container {
   position: relative;
   width: 100%;
-  height: 100svh;
+  min-height: 100svh;
+  gap: 1rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 2rem;
+  padding: 1rem;
 }
 
 .content {
   display: grid;
   gap: 1rem;
+  max-width: 20rem;
 }
 
 .subtitle {
@@ -49,5 +52,9 @@ hr {
 p {
   text-align: center;
   color: var(--color-text-muted);
+}
+
+.logo {
+  width: min(30rem, 100%);
 }
 </style>
